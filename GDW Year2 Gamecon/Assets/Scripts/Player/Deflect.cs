8955e98af14f03be99c8a10d1643138274137acd
christophershin <dinojun323@gmail.com>
2025-09-23 19:24:05 -0400

    parry sound effect

--- GDW Year2 Gamecon/Assets/Scripts/Player/Deflect.cs
+++ GDW Year2 Gamecon/Assets/Scripts/Player/Deflect.cs
@@ -2,25 +2,28 @@ using UnityEngine;
 
 public class Deflect: MonoBehaviour
 {
+    [SerializeField] private AudioClip[] sounds;
+    private AudioSource ManagerAudio;
+
+
+
+
+    private void Start()
+    {
+        ManagerAudio = GetComponent<AudioSource>();
+    }
 
 
 
 
 
-
-
-
-
-
-
-
-   void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
    {
         if(collision.gameObject.CompareTag("Parriable"))
         {
             GameObject obj = collision.gameObject;
             obj.GetComponent<Rigidbody>().linearVelocity = Camera.main.transform.forward * 8;
-
+            ManagerAudio.Play();
         }
    }
 
