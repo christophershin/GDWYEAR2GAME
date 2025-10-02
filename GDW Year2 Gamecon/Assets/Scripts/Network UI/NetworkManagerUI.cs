fd0c676178566def2558ba0a445f875e45069bd1
Gomagan <gomagan.prabagar@ontariotechu.net>
2025-10-02 16:31:43 -0400

    Tried implementing unity servers (didn't test tho)

--- GDW Year2 Gamecon/Assets/Scripts/Network UI/NetworkManagerUI.cs
+++ GDW Year2 Gamecon/Assets/Scripts/Network UI/NetworkManagerUI.cs
@@ -1,29 +1,35 @@
 using System;
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NetworkManagerUI : MonoBehaviour
+public class NetworkManagerUI : TestRelay
 {
-    [SerializeField] private Button serverBtn;
+    //[SerializeField] private Button serverBtn;
     [SerializeField] private Button clientBtn;
     [SerializeField] private Button hostBtn;
+    
+    [SerializeField] private TMP_InputField inputField;
 
     private void Awake()
     {
-        serverBtn.onClick.AddListener((() =>
-        {
-            NetworkManager.Singleton.StartServer();
-        }));
+        // serverBtn.onClick.AddListener((() =>
+        // {
+        //     
+        //     //NetworkManager.Singleton.StartServer();
+        // }));
 
         clientBtn.onClick.AddListener((() =>
                 {
-                    NetworkManager.Singleton.StartClient();
+                    JoinRelay(inputField.text);
+                    //NetworkManager.Singleton.StartClient();
                 }
             ));
         hostBtn.onClick.AddListener((() =>
                 {
-                    NetworkManager.Singleton.StartHost();
+                    CreateRelay();
+                 //   NetworkManager.Singleton.StartHost();
                 }
             ));
     }
