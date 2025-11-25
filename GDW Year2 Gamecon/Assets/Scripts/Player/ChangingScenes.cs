66de34d448ac940386e19d5f5139d5604c08e1be
FynnCoopSchool <fynnian.cooper@ontariotechu.net>
2025-11-25 18:55:32 -0500

    Enemy & Crown

--- GDW Year2 Gamecon/Assets/Scripts/Player/ChangingScenes.cs
+++ GDW Year2 Gamecon/Assets/Scripts/Player/ChangingScenes.cs
@@ -9,43 +9,6 @@ public class ChangingScenes : MonoBehaviour
 {
 
     public string Scene;
-    private bool pauseIsOn = false;
-    [SerializeField] private Canvas PauseMenu;
-
-    private void Start()
-    {
-
-    }
-
-
-
-    private void Update()
-    {
-
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            pauseIsOn = !pauseIsOn;
-
-        }
-
-
-        if (PauseMenu)
-        {
-            if (pauseIsOn)
-            {
-                PauseMenu.enabled = true;
-                Time.timeScale = 0;
-            }
-            else
-            {
-                PauseMenu.enabled = false;
-                Time.timeScale = 1;
-            }
-        }
-
-
-    }
-
 
     public void ChangeSceneTo()
     {
