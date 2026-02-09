8cea58eb9c405811f00c5dcacd7d24469aeeaa26
niteronin <144817730+Gomagan@users.noreply.github.com>
2026-02-09 17:01:22 -0500

    fixed bug i think

--- GDW Year2 Gamecon/Assets/Scripts/Player/PlayerMovementandCamera.cs
+++ GDW Year2 Gamecon/Assets/Scripts/Player/PlayerMovementandCamera.cs
@@ -201,7 +201,7 @@ public class PlayerMovementandCamera : NetworkBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Card") && other.gameObject.name != "null" && other.gameObject.name != "awaiting" && _cardsManager.AddCard(other.gameObject.name))
+        if (other.gameObject.CompareTag("Card") && other.gameObject.name != "null" && other.gameObject.name != "awaiting" && other.gameObject.name != "Card" && _cardsManager.AddCard(other.gameObject.name))
         {
             other.GetComponent<DeleteCard>().DespawnServerRPC();
         }
