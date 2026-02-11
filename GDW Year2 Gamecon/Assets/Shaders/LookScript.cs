efe654c8ee89a3475f266668f4e7138bfefe9a42
niteronin <144817730+Gomagan@users.noreply.github.com>
2026-02-11 14:46:59 -0500

    main menu changes

---
+++ GDW Year2 Gamecon/Assets/Shaders/LookScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class LookScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            transform.LookAt(-hit.point);
+        }
+    }
+}
