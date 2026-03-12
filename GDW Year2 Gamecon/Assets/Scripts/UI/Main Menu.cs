5ef0eeeb17f2deb35d39a2d6dcea9745649f7988
niteronin <144817730+Gomagan@users.noreply.github.com>
2026-03-12 17:31:32 -0400

    changes

--- GDW Year2 Gamecon/Assets/Scripts/UI/Main Menu.cs
+++ GDW Year2 Gamecon/Assets/Scripts/UI/Main Menu.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
@@ -11,8 +13,15 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private TMP_FontAsset defaultFont, horrorFont;
     [SerializeField] private Material platypusEyes;
 
+    [SerializeField]
+    private Light _light;
+    
+    private Coroutine _currentCoroutine;
+
     private void Start()
     {
+        _light.color = Color.white;
+        _light.intensity = 1.0f;
         platypusEyes.SetFloat("_Evil", 0);
     }
 
@@ -26,30 +35,30 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
-    public void PointedAtPlay()
+    public void PointedAtPlay(Image img)
     {
-        PointAtButton();
+        PointAtButton(img.transform.position);
         platypusTalkingText.text = "Lets see your skill";
         SetDefaultFont();
     }
     
-    public void PointedAtAbout()
+    public void PointedAtAbout(Image img)
     {
-        PointAtButton();
+        PointAtButton(img.transform.position);
         platypusTalkingText.text = "Want to know more about the game?";
         SetDefaultFont();
     }
     
-    public void PointedAtOptions()
+    public void PointedAtOptions(Image img)
     {
-        PointAtButton();
+        PointAtButton(img.transform.position);
         platypusTalkingText.text = "Change the settings perhaps?";
         SetDefaultFont();
     }
 
-    public void PointedAtQuit()
+    public void PointedAtQuit(Image img)
     {
-        PointAtButton();
+        PointAtButton(img.transform.position);
         platypusTalkingText.text = "Do you really want to leave?";
         SetHorrorFont();
     }
@@ -68,6 +77,8 @@ public class MainMenu : MonoBehaviour
 
     private void SetHorrorFont()
     {
+        _light.color = Color.red;
+        _light.intensity = 3.0f;
         platypusEyes.SetFloat("_Evil",1);
         platypusTalkingText.font = horrorFont;
         platypusTalkingText.color = Color.red;
@@ -76,23 +87,53 @@ public class MainMenu : MonoBehaviour
 
     private void SetDefaultFont()
     {
+        _light.color = Color.white;
+        _light.intensity = 1.0f;
         platypusEyes.SetFloat("_Evil", 0);
         platypusTalkingText.font = defaultFont;
         platypusTalkingText.color = Color.white;
     }
 
-    private void PointAtButton()
+    private void PointAtButton(Vector3 position)
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = Camera.main.ScreenPointToRay(position);
         RaycastHit hit;
-        
         if (Physics.Raycast(ray, out hit))
         {
             if (hit.transform.gameObject.CompareTag("point"))
             {
-                platypusHandTransform.LookAt(hit.point);
+                // platypusHandTransform.LookAt(hit.point);
+                if (_currentCoroutine != null)
+                    StopCoroutine(_currentCoroutine);
+                
+                _currentCoroutine =  StartCoroutine(LerpLookAt(platypusHandTransform, hit.point, 0.08f));
             }
             
         }
+    }
+    
+    IEnumerator LerpLookAt(Transform platypusHandTransform, Vector3 targetPoint, float duration)
+    {
+        Quaternion startRotation = platypusHandTransform.rotation;
+        Vector3 direction = targetPoint - platypusHandTransform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+        float time = 0f;
+
+        while (time < duration)
+        {
+            float t = time / duration;
+            platypusHandTransform.rotation = Quaternion.Slerp(startRotation, targetRotation, t);
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        platypusHandTransform.rotation = targetRotation;
+    }
+
+    private void Update()
+    {
+        
     }
 }
